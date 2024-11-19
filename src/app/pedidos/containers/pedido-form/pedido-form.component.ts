36ba32fadd54a2e9ec5333eb6afb8d47ef941dbf
ricardo <ric.vecchio@gmail.com>
2024-11-19 09:27:48 -0300

    Incluindo formatação do CPF/CNPJ do pedido.

--- src/app/pedidos/containers/pedido-form/pedido-form.component.ts
+++ src/app/pedidos/containers/pedido-form/pedido-form.component.ts
@@ -295,6 +295,33 @@ export class PedidoFormComponent implements OnInit {
     });
   }
 
+  onCpfCnpjPedidoInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\D/g, '');
+
+    if (value.length > 14) {
+      value = value.substring(0, 14);
+    }
+
+    if (value.length > 11) {
+      // Formata como CNPJ: xx.xxx.xxx/xxxx-xx
+      value = value.replace(
+        /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2}).*/,
+        '$1.$2.$3/$4-$5',
+      );
+    } else if (value.length > 9) {
+      // Formata como CPF: xxx.xxx.xxx-xx
+      value = value.replace(/^(\d{3})(\d{3})(\d{3})(\d{2}).*/, '$1.$2.$3-$4');
+    } else if (value.length > 6) {
+      value = value.replace(/^(\d{3})(\d{3})(\d+).*/, '$1.$2.$3');
+    } else if (value.length > 3) {
+      value = value.replace(/^(\d{3})(\d+).*/, '$1.$2');
+    }
+
+    input.value = value;
+    this.formulario.get('cpfcnpj')?.setValue(value);
+  }
+
   setFocus() {
     this.focusElement.nativeElement.focus();
   }
