3fb69d6818f9ddc22321b80168eae0f85d0a14ca
ricardo <ric.vecchio@gmail.com>
2025-01-04 19:47:03 -0300

    ref Render api

--- src/app/clientes/servicos/cliente.service.ts
+++ src/app/clientes/servicos/cliente.service.ts
@@ -9,7 +9,7 @@ import { ClientePagina } from '../../modelo/cliente-pagina';
   providedIn: 'root',
 })
 export class ClienteService {
-  private readonly API = '/transp-api-crud-spring.onrender.com/api/clientes';
+  private readonly API = 'https://transp-api-crud-spring.onrender.com/api/clientes';
   // private readonly API = '/api/clientes';
 
   constructor(private http: HttpClient) {}
