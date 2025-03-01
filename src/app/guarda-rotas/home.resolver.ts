c57929c23b7a9d851f148a2c3e7a72dda9cbce92
ricardo <ric.vecchio@gmail.com>
2025-03-01 14:02:35 -0300

    ajustando home Resolver

--- src/app/guarda-rotas/home.resolver.ts
+++ src/app/guarda-rotas/home.resolver.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { UsuarioService } from '../usuarios/usuario.service';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,8 @@ export class HomeResolver {
 
   constructor(private service: UsuarioService) {}
 
-  resolve(): Observable<any> {
-    return this.service.buscarPorUsername(this.usuario);
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
+    const username = route.paramMap.get('username'); // Ou obter de outro lugar
+    return this.service.buscarPorUsername(username || '');
   }
 }
