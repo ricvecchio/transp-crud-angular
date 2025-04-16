1167caec2c06cfd421b1184851ac092a52b15c68
ricardodelvecchio <ric.vecchio@gmail.com>
2025-04-15 21:36:12 -0300

    removendo imports app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,10 +20,6 @@ import { NovoUsuarioComponent } from './home/login/novo-usuario/novo-usuario.com
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 
-import { NgChartsModule } from 'ng2-charts';  
-import { MetricasComponent } from './metricas/metricas.component';
-
-
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
@@ -40,8 +36,6 @@ import { MetricasComponent } from './metricas/metricas.component';
     NovoUsuarioComponent,
     MenuComponent,
     MensagemComponent,
-    MetricasComponent,
-    NgChartsModule,
   ],
   providers: [
     provideAnimations(),
