d07ef1fa4ac8a57e80156cc4dc46b531e5e1b7b4
ricardodelvecchio <ric.vecchio@gmail.com>
2025-07-06 13:04:14 -0300

    ajustando impressão

--- src/app/pedidos/servico/pedido.service.ts
+++ src/app/pedidos/servico/pedido.service.ts
@@ -204,107 +204,66 @@ export class PedidoService {
   // }
 
 //   // ==> NOVO MÉTODO
-//   async gerarImpressaoUsandoImagem(imagemData: string): Promise<void> {
-//         console.time('PedidoService.gerarImpressaoUsandoImagem'); // EXCLUIR
-//     console.log('→ INÍCIO: gerarImpressaoUsandoImagem'); // EXCLUIR
-//   try {
-//     const iframe = document.createElement('iframe');
-//     iframe.style.position = 'absolute';
-//     iframe.style.width = '0px';
-//     iframe.style.height = '0px';
-//     iframe.style.border = 'none';
-//     document.body.appendChild(iframe);
+  async gerarImpressaoUsandoImagem(imagemData: string): Promise<void> {
+        console.time('PedidoService.gerarImpressaoUsandoImagem'); // EXCLUIR
+    console.log('→ INÍCIO: gerarImpressaoUsandoImagem'); // EXCLUIR
+  try {
+    const iframe = document.createElement('iframe');
+    iframe.style.position = 'absolute';
+    iframe.style.width = '0px';
+    iframe.style.height = '0px';
+    iframe.style.border = 'none';
+    document.body.appendChild(iframe);
 
-//     const iframeDocument = iframe.contentWindow?.document;
-//     if (!iframeDocument) {
-//       this.mensagemService.showErrorMessage(
-//         'Erro ao acessar o documento do iframe',
-//       );
-//       return;
-//     }
-
-//     iframeDocument.open();
-//     iframeDocument.write(`
-//       <html>
-//         <head>
-//           <style>
-//             @page { size: A4 portrait; margin: 0; }
-//             body { margin: 0; display: flex; flex-direction: column; height: 100vh; }
-//             .page { position: relative; width: 100%; height: 100vh; }
-//             .image-container { width: 100%; height: 50%; position: absolute; padding: 20px; box-sizing: border-box; display: flex; justify-content: center; align-items: center; }
-//             .image { width: 100%; height: 100%; object-fit: contain; position: relative; }
-//           </style>
-//         </head>
-//         <body>
-//           <div class="page">
-//             <div class="image-container" style="top: 0;">
-//               <img src="${imagemData}" class="image" />
-//             </div>
-//             <div class="image-container" style="top: 50%;">
-//               <img src="${imagemData}" class="image" />
-//             </div>
-//           </div>
-//         </body>
-//       </html>
-//     `);
-//     iframeDocument.close();
-
-//     await new Promise<void>((resolve) => {
-//       iframe.onload = () => {
-//         iframe.contentWindow?.focus();
-//         iframe.contentWindow?.print();
-//         iframe.contentWindow?.addEventListener('afterprint', () => {
-//           document.body.removeChild(iframe);
-//           resolve();
-//         });
-//       };
-//     });
-//         console.log('← FIM: gerarImpressaoUsandoImagem'); //EXCLUIR
-//     console.timeEnd('PedidoService.gerarImpressaoUsandoImagem'); //EXCLUIR
-//   } catch (error) {
-//     this.mensagemService.showErrorMessage('Erro ao imprimir o pedido.');
-//   }
-// }
-
-gerarImpressaoUsandoImagem(imagemBase64: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const janelaImpressao = window.open('', '_blank');
-    if (janelaImpressao) {
-janelaImpressao.document.write(`
-  <html>
-    <head>
-      <title>Impressão do Pedido</title>
-    </head>
-    <body style="margin: 0; padding: 0;">
-      <img id="imagemPedido" src="data:image/png;base64,${imagemBase64}" style="width:100%;"/>
-      <script>
-        const img = document.getElementById('imagemPedido');
-        img.onload = function () {
-          setTimeout(() => {
-            window.print();
-          }, 300); // aguarda o render da imagem antes de imprimir
-        };
-        window.onafterprint = function () {
-          window.close();
-        };
-      </script>
-    </body>
-  </html>
-`);
-
-      janelaImpressao.document.close();
-
-      const timer = setInterval(() => {
-        if (janelaImpressao.closed) {
-          clearInterval(timer);
-          resolve();
-        }
-      }, 500);
-    } else {
-      reject('Não foi possível abrir a janela de impressão');
+    const iframeDocument = iframe.contentWindow?.document;
+    if (!iframeDocument) {
+      this.mensagemService.showErrorMessage(
+        'Erro ao acessar o documento do iframe',
+      );
+      return;
     }
-  });
-}
 
+    iframeDocument.open();
+    iframeDocument.write(`
+      <html>
+        <head>
+          <style>
+            @page { size: A4 portrait; margin: 0; }
+            body { margin: 0; display: flex; flex-direction: column; height: 100vh; }
+            .page { position: relative; width: 100%; height: 100vh; }
+            .image-container { width: 100%; height: 50%; position: absolute; padding: 20px; box-sizing: border-box; display: flex; justify-content: center; align-items: center; }
+            .image { width: 100%; height: 100%; object-fit: contain; position: relative; }
+          </style>
+        </head>
+        <body>
+          <div class="page">
+            <div class="image-container" style="top: 0;">
+              <img src="${imagemData}" class="image" />
+            </div>
+            <div class="image-container" style="top: 50%;">
+              <img src="${imagemData}" class="image" />
+            </div>
+          </div>
+        </body>
+      </html>
+    `);
+    iframeDocument.close();
+
+    await new Promise<void>((resolve) => {
+      iframe.onload = () => {
+        iframe.contentWindow?.focus();
+        iframe.contentWindow?.print();
+        iframe.contentWindow?.addEventListener('afterprint', () => {
+          document.body.removeChild(iframe);
+          resolve();
+        });
+      };
+    });
+        console.log('← FIM: gerarImpressaoUsandoImagem'); //EXCLUIR
+    console.timeEnd('PedidoService.gerarImpressaoUsandoImagem'); //EXCLUIR
+  } catch (error) {
+    this.mensagemService.showErrorMessage('Erro ao imprimir o pedido.');
+  }
+}
 
 }
